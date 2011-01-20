fdadf3d682f73b02bec35d6c3eb865b9c2bf0f24
Simo Leone <leone.simo@gmail.com>
2011-01-20 15:58:19 -0800

    add ldap authentication

--- weTunes/settings.example.py
+++ weTunes/settings.example.py
@@ -94,3 +94,15 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+# authenticate against ldap but allow us to overlay groups
+# and permissions
+AUTH_LDAP_SERVER_URI = "ldap://ldap.example.com"
+AUTH_LDAP_BIND_DN = ""
+AUTH_LDAP_BIND_PASSWORD = ""
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=example,dc=com",
+        ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
