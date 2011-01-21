11a0cce66613f1ff1303c836097745f0334d93f4
Alex Paul <alex@alexpaul.net>
2011-01-21 04:46:10 -0800

    algorithm 2.0

--- weTunes/frontend/models.py
+++ weTunes/frontend/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Vote(models.Model):
+    block = models.ForeignKey('Block')
     user = models.CharField(max_length = 128)
+    created = models.DateField(auto_now = True)
+    
+class Block(models.Model):
+    length = models.IntegerField()
+
+class Track(models.Model):
+    block = models.ForeignKey('Block')
     filename = models.CharField(max_length = 1024)
-    updated = models.DateField(auto_now = True)
-    played = models.BooleanField(default = False)
+    track_number = models.IntegerField()
