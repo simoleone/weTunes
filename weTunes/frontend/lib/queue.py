de3e542b823658feb4b7128cee9b3f4f3d6c72f4
Alex Paul <alex@alexpaul.net>
2011-01-20 19:17:46 -0800

    probly doesn't work

--- weTunes/frontend/lib/queue.py
+++ weTunes/frontend/lib/queue.py
@@ -1,21 +1,31 @@
 import frontend.models
+from frontend.lib.mpc import MPC
 
 class Queue:
     def __init__(self):
+        self.queue = None
         self.playlist = None
 
-    def generate_queue(self):
+    def get_playlist(self):
         if self.playlist is not None:
             return self.playlist
+        for filename in queue:
+
+        return self.playlist
+
+
+    def get_queue(self):
+        if self.queue is not None:
+            return self.queue
         
         unplayed_votes = Vote.objects.filter(played__exact = False)
-        self.tracks = {}     # songid => set(self.users_voted)
-        self.users = {}      # user => (last_vote_played_timestamp, set(songids_voted))
-        self.playlist = []  # songid, in order
+        self.tracks = {}     # filename => set(self.users_voted)
+        self.users = {}      # user => (last_vote_played_timestamp, set(filenames_voted))
+        self.queue = []  # filename, in order
+        self.playlist = []
         self.timestamp = datetime.now()
         for v in unplayed_votes:
-            self.process_vote(v)
-
+            self.analyze_vote(v)
 
         while len(self.tracks) > 0:
             # check if a track has more votes than the rest
@@ -29,7 +39,7 @@ class Queue:
                     fav_tracks.add(t)
             if fav_tracks is None or len(fav_tracks) == 0:
                 # no tracks with a positive vote
-                return self.playlist
+                return self.queue
             if len(fav_tracks) == 1:
                 self.emit_track(fav_tracks.pop())
             else:
@@ -38,53 +48,46 @@ class Queue:
                 selected_track = None
                 for t in fav_tracks:
                     for u in self.tracks[t]:
-                        (user_timestamp, songids) = self.users[u]
+                        (user_timestamp, filenamess) = self.users[u]
                         if last_play_timestamp is None or user_timestamp < last_play_timestamp:
                             last_play_timestamp = user_timestamp
                             selected_track = t
                 if selected_track is None:
-                    return self.playlist
+                    return self.queue
                 else:
                     self.emit_track(selected_track)
         
-        return self.playlist
+        return self.queue
 
-
-
-
-    def emit_track(self, songid):
-        self.playlist.append(songid)
-        users_voted = self.tracks[songid]
+    def emit_track(self, filename):
+        users_voted = self.tracks[filename]
+        self.queue.append(filename)
+        song = MPC().search('file', filename)[0]
+        song['votes'] = len(users_voted)
+        song['voters'] = users_voted
+        self.playlist.append(song)
         for u in users_voted:
-            (last_play_timestamp, songids_voted) = self.users[u]
-            songids_voted.discard(songid)
+            (last_play_timestamp, filenames_voted) = self.users[u]
+            filenames_voted.discard(filename)
             # set the user's last play to be now (i.e. when this track will be played)
-            self.users[u] = (self.timestamp, songids_voted)
-        del tracks[songid]
+            self.users[u] = (self.timestamp, filenames_voted)
+        del tracks[filename]
         self.timestamp = self.timestamp + datetime.timedelta(minutes=3)
 
-
-
-
-            
-
-
-
-
-    def process_vote(self, v):
+    def analyze_vote(self, v):
         if v.user in self.users:
-            (last_play_timestamp, songids_voted_up) = self.users[v.user]
+            (last_play_timestamp, filenames_voted_up) = self.users[v.user]
         else:
-            songids_voted_up = set()
+            filenames_voted_up = set()
             try:
                 last_play = Vote.objects.filter(played__exact = True, user__exact = v.user, upvote__exact = True).order_by('-updated')[0:1].get()
                 last_play_timestamp = last_play.updated
             except DoesNotExist:
                 last_play_timestamp = None
-            self.users[v.user] = (last_play_timestamp, songids_voted_up)
-        songids_voted_up.add(v.songid)
-        if v.songid in self.tracks:
-            self.tracks[v.songid].add(v.user)
+            self.users[v.user] = (last_play_timestamp, filenames_voted_up)
+        filenames_voted_up.add(v.filename)
+        if v.filename in self.tracks:
+            self.tracks[v.filename].add(v.user)
         else
-            self.tracks[v.songid] = set(v.user)
+            self.tracks[v.filename] = set(v.user)
 
