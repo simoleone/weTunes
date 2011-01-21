a0ced27a93f694167cccfe2910e189084a519621
Simo Leone <leone.simo@gmail.com>
2011-01-20 16:49:04 -0800

    stub out some views

--- weTunes/frontend/views.py
+++ weTunes/frontend/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    t = loader.get_template('index.html')
+    c = Context({
+        'song': song,
+        'volume': volume,
+        'playlist', playlist
+    })
+    return HttpResponse(t.render(c))
+
+def search(request):
+    return False
+
+@login_required
+def vote(request):
+    return False
+
+@login_required
+def changevolume(request):
+    return False
+
+@login_required
+def playpause(request):
+    return False
