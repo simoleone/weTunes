fe23d20146d203b015a325b2e8659bfc2103347b
Alex Paul <alex@alexpaul.net>
2011-01-21 05:55:20 -0800

    queue implementation

--- weTunes/frontend/lib/queue.py
+++ weTunes/frontend/lib/queue.py
@@ -1,127 +1,68 @@
-from frontend.models import Vote
+from frontend.models import Vote, Block, Track
 from frontend.lib.mpc import MPC
 from datetime import datetime, timedelta
 
 class Queue:
     def __init__(self):
-        self.queue = None
-        self.playlist = None
+        pass
 
-    def get_playlist(self):
-        self.get_queue()
-        return self.playlist
+    def compute_queue(self):
+        queue = []
+        # start with the current block
+        current_song_id = MPC().currentsong()['id']
+        current_track = Track.objects.get(playlist_id = current_song_id)
+        queue.append(current_track)
+        tracks_in_block = list(Track.objects.filter(block = current_track.block, track_number__gt = current_track.track_number).order_by('track_number'))
+        queue += tracks_in_block
 
-    def get_queue(self):
-        if self.queue is not None:
-            return self.queue
-
-        self.update_played()
-        
-        unplayed_votes = Vote.objects.filter(played__exact = False)
-        self.tracks = {}     # filename => set(self.users_voted)
-        self.users = {}      # user => (last_vote_played_timestamp, set(filenames_voted))
-        self.queue = []  # filename, in order
-        self.playlist = []
-        self.timestamp = datetime.now()
-        for v in unplayed_votes:
-            self.analyze_vote(v)
-
-        while len(self.tracks) > 0:
-            # check if a track has more votes than the rest
-            max_votes = 0
-            fav_tracks = None
-            for t in self.tracks:
-                if fav_tracks is None or len(self.tracks[t]) > max_votes:
-                    fav_tracks = set()
-                    fav_tracks.add(t)
-                    max_votes = len(self.tracks[t])
-                elif len(self.tracks[t]) == max_votes:
-                    fav_tracks.add(t)
-            if fav_tracks is None or len(fav_tracks) == 0:
-                # no tracks with a positive vote
-                return self.queue
-            elif len(fav_tracks) == 1:
-                self.emit_track(fav_tracks.pop())
-            else:
-                # now find the user who's vote was played longest ago
-                last_play_timestamp = None
-                selected_track = None
-                for t in fav_tracks:
-                    for u in self.tracks[t]:
-                        (user_timestamp, filenamess) = self.users[u]
-                        if last_play_timestamp is None or user_timestamp < last_play_timestamp:
-                            last_play_timestamp = user_timestamp
-                            selected_track = t
-                if selected_track is None:
-                    return self.queue
-                else:
-                    self.emit_track(selected_track)
-        
-        return self.queue
-
-    def emit_track(self, filename):
-        users_voted = self.tracks[filename]
-        self.queue.append(filename)
-        try:
-            song = MPC().search('file', filename)[0]
-            song['votes'] = len(users_voted)
-            song['voters'] = users_voted
-            self.playlist.append(song)
-        except IndexError:
-            print "Could not find file %s" % filename
-
-        for u in users_voted:
-            (last_play_timestamp, filenames_voted) = self.users[u]
-            filenames_voted.discard(filename)
-            # set the user's last play to be now (i.e. when this track will be played)
-            self.users[u] = (self.timestamp, filenames_voted)
-        del self.tracks[filename]
-        self.timestamp = self.timestamp + timedelta(minutes=3)
-
-    def analyze_vote(self, v):
-        if v.user in self.users:
-            (last_play_timestamp, filenames_voted_up) = self.users[v.user]
-        else:
-            filenames_voted_up = set()
-            try:
-                last_play = Vote.objects.filter(played__exact = True, user__exact = v.user).order_by('-updated')[0:1].get()
-                last_play_timestamp = last_play.updated
-            except Vote.DoesNotExist:
-                last_play_timestamp = None
-            self.users[v.user] = (last_play_timestamp, filenames_voted_up)
-        filenames_voted_up.add(v.filename)
-        if v.filename not in self.tracks:
-            self.tracks[v.filename] = set()
-        self.tracks[v.filename].add(v.user)
+        blocks = sorted(list(Block.objects.exclude(id = current_track.block)), Block.priority_score)
+        for b in blocks:
+            tracks_in_block = list(Track.objects.filter(block = b).order_by('track_number'))
+            queue += tracks_in_block
+        print queue
+        return queue
 
     def save_queue(self):
-        self.get_queue()
-        # clear everything from MPC's queue except the current song
-        current_song = MPC().currentsong()
-        current_playlist = MPC().playlistid()
-        if not current_song or 'file' not in current_song:
-            current_song = {'file': None}
-        for s in current_playlist:
-            if s['file'] != current_song['file']:
-                MPC().deleteid(s['id'])
+        queue = self.compute_queue()
+        playlist = MPC().playlistid()
+    
+        #trim the head of the list
+        while len(playlist) > 0 and playlist[0]['id'] != queue[0].playlist_id:
+            MPC().deleteid(playlist[0]['id'])
+            playlist.pop(0)
 
-        for filename in self.queue:
-            MPC().findadd('file', filename)
+        #XXX FIXME TODO finish this
 
-    def update_played(self):
-        current_playlist = MPC().playlistinfo()
-        current_song = MPC().currentsong()
-        if not current_song or 'file' not in current_song:
-            return
-        played_songs = []
-        for s in current_playlist:
-            played_songs.append(s)
-            if s['file'] == current_song['file']:
-                # we've reached the currently-playing song
-                self.mark_played(played_songs)
-                return
+            
 
-    def mark_played(self, played_songs):
-        for s in played_songs:
-            Vote.objects.filter(played__exact = False, filename__exact = s['file']).update(played = True)
+#    def save_queue(self):
+#        self.get_queue()
+#        # clear everything from MPC's queue except the current song
+#        current_song = MPC().currentsong()
+#        current_playlist = MPC().playlistid()
+#        if not current_song or 'file' not in current_song:
+#            current_song = {'file': None}
+#        for s in current_playlist:
+#            if s['file'] != current_song['file']:
+#                MPC().deleteid(s['id'])
+#
+#        for filename in self.queue:
+#            MPC().findadd('file', filename)
+#
+#    def update_played(self):
+#        current_playlist = MPC().playlistinfo()
+#        current_song = MPC().currentsong()
+#        if not current_song or 'file' not in current_song:
+#            return
+#        played_songs = []
+#        for s in current_playlist:
+#            played_songs.append(s)
+#            if s['file'] == current_song['file']:
+#                # we've reached the currently-playing song
+#                self.mark_played(played_songs)
+#                return
+#
+#    def mark_played(self, played_songs):
+#        for s in played_songs:
+#            Vote.objects.filter(played__exact = False, filename__exact = s['file']).update(played = True)
 
