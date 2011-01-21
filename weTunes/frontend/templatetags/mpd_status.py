cace6ac223658a0afd7d96fc53a161932f0f142a
Simo Leone <leone.simo@gmail.com>
2011-01-20 22:12:37 -0800

    add mpd status to all pages

---
+++ weTunes/frontend/templatetags/mpd_status.py
@@ -0,0 +1,9 @@
+from django import template
+register = template.Library()
+
+from frontend.lib.mpc import MPC
+
+# just renders the currently playing song, volume, and so on
+@register.inclusion_tag('templatetags/mpd_status.html')
+def mpd_status():
+    return MPC().status()
