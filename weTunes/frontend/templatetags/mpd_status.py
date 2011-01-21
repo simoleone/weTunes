4f435fabeacf40df0f14c0f633103e19de4588f9
Simo Leone <leone.simo@gmail.com>
2011-01-21 04:20:49 -0800

    get rid of templatetag for mpd controller

--- weTunes/frontend/templatetags/mpd_status.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-from frontend.lib.mpc import MPC
-
-# just renders the currently playing song, volume, and so on
-@register.inclusion_tag('templatetags/mpd_status.html')
-def mpd_status():
-    return None
