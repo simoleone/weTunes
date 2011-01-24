b83ac44a68d22d1a22481a34b3b33390adfafb93
Simo Leone <leone.simo@gmail.com>
2011-01-24 01:29:04 -0800

    Implement queueing and syncing to mpd playlist
    Queue is based on "blocks" of songs, which are ranked based on the the
    formula:
                sum(vote ages)
    priority = ---------------
                block length
    
    Higher priority score wins.

--- weTunes/frontend/lib/queue.py
+++ weTunes/frontend/lib/queue.py
@@ -1,32 +1,12 @@
 from frontend.models import Vote, Block, Track
 from frontend.lib.mpc import MPC
-from datetime import datetime, timedelta
+from datetime import datetime
 
 class Queue:
-    def __init__(self):
-        self.compute_queue()
-        pass
 
-    def compute_queue(self):
-        queue = []
-        # start with the current block
-        current_song_id = MPC().currentsong()['id']
-        current_track = Track.objects.get(playlist_id = current_song_id)
-        queue.append(current_track)
-        tracks_in_block = list(Track.objects.filter(block = current_track.block, track_number__gt = current_track.track_number).order_by('track_number'))
-        queue += tracks_in_block
-
-        blocks = sorted(list(Block.objects.exclude(id = current_track.block)), Block.priority_score)
-        for b in blocks:
-            tracks_in_block = list(Track.objects.filter(block = b).order_by('track_number'))
-            queue += tracks_in_block
-        print queue
-        return queue
-
+    """Sync the current calculated queue to mpd's queue"""
     def save_queue(self):
-        queue = self.compute_queue()
-
-        validids = set()
+        queue = self.__compute_queue()
     
         for i in range(len(queue)):
             if queue[i].playlist_id is not None:
@@ -34,15 +14,65 @@ class Queue:
             else:
                 queue[i].playlist_id = MPC().addid(queue[i].filename, i)
                 queue[i].save()
-            validids.add(queue[i].playlist_id)
 
-        playlist = MPC().playlistid()
-        for track in playlist:
-            if track['id'] not in validids:
-                MPC().deleteid(track['id'])
-                t = Track.objects.get(playlist_id = track['id'])
-                blockid = t.block
-                t.delete()
-                if Track.objects.count(block = blockid) == 0:
-                    Vote.objects.delete(block = blockid)
-                    Block.objects.delete(id = blockid)
+    """Compute the current queue based on votes for blocks and
+       currently playing block.
+
+       returns a list of Tracks
+    """
+    def __compute_queue(self):
+        # 1. determine current block
+        # 2. calculate scores for all other blocks
+        # 3. output tracks in order: current block, then remaining blocks by descending score
+
+        play_queue = []
+        blocks_to_score = []
+        cursong_id = self.__clean_playlist()
+
+        if cursong_id == None:
+            # play queue has never been saved to mpd
+            # calculate scores for all blocks
+            blocks_to_score = list(Block.objects.all())
+        else:
+            # add tracks in the current block to the queue
+            curblock = Track.objects.filter(playlist_id = cursong_id)[0].block
+            curblock_tracks = curblock.track_set.all().order_by('track_number')
+            play_queue += list(curblock.track_set.all().order_by('track_number'))
+            # and score the rest...
+            blocks_to_score = list(Block.objects.exclude(id = curblock.id))
+
+        # sort them by score
+        d = datetime.now()
+        blocks_to_score.sort(key=lambda x : Block.priority_score(x, d))
+
+        # add remaining tracks to the queue
+        for b in blocks_to_score:
+            play_queue += list(b.track_set.all().order_by('track_number'))
+
+        return play_queue
+
+    """Remove any already played songs from playlist and database
+       returns playlist id of currently playing song or None
+    """
+    def __clean_playlist(self):
+        status = MPC().status()
+        plinfo = MPC().playlistinfo()
+
+        if status['playlistlength'] == '0':
+            # no queue has been synced to mpd yet
+            return None
+        elif not status.has_key('songid'):
+            # either play has not started, or something is wrong
+            #  we'll assume play hasn't started and return the first song
+            #  since we know the playlist is non-zero in length
+            return plinfo[0]['id']
+
+        # delete everything up to the current song
+        for song in plinfo:
+            songid = song['id']
+            if songid == status['songid']:
+                break
+            MPC().deleteid(songid)
+            Track.objects.filter(playlist_id = songid).delete()
+
+        return status['songid']
--- weTunes/frontend/models.py
+++ weTunes/frontend/models.py
@@ -2,33 +2,27 @@ from django.db import models
 from datetime import datetime, timedelta
 from frontend.lib.mpc import MPC
 
-# Create your models here.
-
 class Vote(models.Model):
     block = models.ForeignKey('Block')
     user = models.CharField(max_length = 128)
-    created = models.DateField(auto_now_add = True)
+    created = models.DateTimeField(auto_now_add = True)
     
 class Block(models.Model):
     length = models.IntegerField()
 
     def priority_score(self, when = None):
-        if self.priority is not None:
-            return self.priority
-        if self.length == 0:
-            return 0
         # score = sum(wait time) / length
         if when is None:
             when = datetime.now()
-        score = timedelta.timedelta()
-        votes = Vote.objects.filter(block__exact = self)
+        score = timedelta()
+        votes = self.vote_set.all()
         for v in votes:
             score += (when - v.created)
-        self.priority = int(score) / float(self.length)
+        self.priority = score.total_seconds() / float(self.length)
         return self.priority
 
     def update_length(self):
-        tracks = Track.objects.filter(block__exact = self)
+        tracks = self.track_set.all()
         time = 0
         for t in tracks:
             time += int(MPC().find('file', t.filename)[0]['time'])
@@ -37,7 +31,16 @@ class Block(models.Model):
 
 
 class Track(models.Model):
+    # TODO: make track removals cascade to blocks and votes
     block = models.ForeignKey('Block')
     filename = models.CharField(max_length = 1024)
     track_number = models.IntegerField()
     playlist_id = models.IntegerField(null = True)
+
+    def delete(self):
+        super.delete()
+
+        # ensure that empty blocks are cleaned up
+        if self.block.track_set.all().count() == 0:
+            self.block.vote_set.delete()
+            self.block.delete()
