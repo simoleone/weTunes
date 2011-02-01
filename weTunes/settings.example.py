ca4e9478d1efe8f002ad9430784d2d9aa112860b
Simo Leone <leone.simo@gmail.com>
2011-02-01 01:19:28 -0800

    make login redirect somewhere that actually exists

--- weTunes/settings.example.py
+++ weTunes/settings.example.py
@@ -79,6 +79,8 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'weTunes.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
